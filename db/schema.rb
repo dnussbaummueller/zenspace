c579a186c4ece477c7895493c43aa80306074d2d
Anton Bayborosha <anton.bayborosha@gmail.com>
2023-03-06 12:46:50 +0100

    schema comit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_112225) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_113740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "yoga_class_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["yoga_class_id"], name: "index_bookings_on_yoga_class_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teachers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +51,46 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_112225) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "yoga_classes", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer "capacity"
+    t.bigint "yoga_studio_teacher_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_yoga_classes_on_user_id"
+    t.index ["yoga_studio_teacher_id"], name: "index_yoga_classes_on_yoga_studio_teacher_id"
+  end
+
+  create_table "yoga_studio_teachers", force: :cascade do |t|
+    t.bigint "yoga_studio_id", null: false
+    t.bigint "teacher_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["teacher_id"], name: "index_yoga_studio_teachers_on_teacher_id"
+    t.index ["yoga_studio_id"], name: "index_yoga_studio_teachers_on_yoga_studio_id"
+  end
+
+  create_table "yoga_studios", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_yoga_studios_on_user_id"
+  end
+
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "yoga_classes"
+  add_foreign_key "teachers", "users"
+  add_foreign_key "yoga_classes", "users"
+  add_foreign_key "yoga_classes", "yoga_studio_teachers"
+  add_foreign_key "yoga_studio_teachers", "teachers"
+  add_foreign_key "yoga_studio_teachers", "yoga_studios"
+  add_foreign_key "yoga_studios", "users"
 end
