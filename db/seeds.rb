dbf51df5c3cb772486f0ec5a026797ca1adbbd83
Daniel Nussbaummueller <d.nussbaummueller@gmail.com>
2023-03-06 14:31:47 +0100

    first draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Destroying the database
+puts "Destroying the database..."
+Booking.destroy_all
+YogaClass.destroy_all
+YogaStudioTeacher.destroy_all
+Review.destroy_all
+YogaStudio.destroy_all
+Teacher.destroy_all
+User.destroy_all
+
+# Creating users
+puts "Creating users..."
+
+user1 = User.create!(
+  email: "user1@gmail.com",
+  password: "123456"
+)
+
+user2 = User.create!(
+  email: "user2@gmail.com",
+  password: "123456"
+)
+
+# Creating teachers
+puts "Creating teachers..."
+
+teacher1 = Teacher.create!(
+  name: "Teacher 1",
+  description: "Teacher 1 description",
+  user: user1
+)
+
+teacher2 = Teacher.create!(
+  name: "Teacher 2",
+  description: "Teacher 2 description",
+  user: user1
+)
+
+# Creating Yogastudios
+puts "Creating yoga studios..."
+
+yoga_studio1 = YogaStudio.create!(
+  name: "Yoga Studio 1",
+  description: "Yoga Studio 1 description",
+  address: "Yoga Studio 1 address",
+  user: user1
+)
