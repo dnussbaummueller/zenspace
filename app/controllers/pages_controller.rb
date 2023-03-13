752570f1bb79768fc3d027f22980d8bd2911307f
Daniel Nussbaummueller <d.nussbaummueller@gmail.com>
2023-03-13 14:19:09 +0100

    improved some details and implemented the admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,8 @@ class PagesController < ApplicationController
   end
 
   def admin
-    if current_user.admin?
-      @yoga_studios = YogaStudio.all
-      @yoga_classes = YogaClass.all
-      @users = User.all
-      @teachers = Teacher.all
-    else
-      redirect_to root_path
-    end
+      @yoga_studios = YogaStudio.where(user: current_user)
+      @yoga_classes = YogaClass.where(user: current_user)
+      @teachers = Teacher.where(user: current_user)
   end
 end
