aefd3943cfe35f5ecbba2abbad72363faf11e3e8
Anton Bayborosha <anton.bayborosha@gmail.com>
2023-03-13 12:56:48 +0100

    teacher and studios validation complieted

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,7 +2,18 @@ class Teacher < ApplicationRecord
   belongs_to :user
   has_many :reviews, as: :reviewable
   has_many_attached :photos
-  
+
   has_many :yoga_studio_teachers
   has_many :yoga_classes, through: :yoga_studio_teachers
+
+  # validation
+  validates :name, presence: true, length: { in: 3..50 }
+  validates :description, presence: true, length: { in: 20..800 }
+  validate :photos_type
+
+  def photos_type
+    if photos.attached? == false
+      errors.add(:photos, ": at least one photo should be attached!")
+    end
+  end
 end
--- app/models/yoga_class.rb
+++ app/models/yoga_class.rb
@@ -15,7 +15,7 @@ class YogaClass < ApplicationRecord
 
   belongs_to :user
   belongs_to :yoga_studio_teacher
-  
+
   has_one :yoga_studio, through: :yoga_studio_teacher
   has_one :teacher, through: :yoga_studio_teacher
   has_one_attached :photo
@@ -24,6 +24,23 @@ has_many :bookings, dependent: :destroy
   validates :style, inclusion: { in: %w[Hatha Vinyasa Ashtanga Iyengar Hot Kundalini Restorative Yin Power Anusara Prenatal Postnatal Jivamukti Other] }
   validates :difficulty, inclusion: { in: ["Beginner", "Intermediate", "Advanced", "All Levels"] }
 
+  # validation ===================================================>
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :price, presence: true, numericality: true, format: { with: /\A\d+[.,]{0,1}\d*\z/ }, comparison: { greater_than: 0 }
+  validates :description, presence: true, length: { in: 20..800 }
+  validates :start_time, presence: true
+  validates :end_time, presence: true, comparison: { greater_than: :start_time }
+  validates :capacity, presence: true, comparison: { greater_than: 0 }, numericality: true
+  validates :style, presence: true, length: { minimum: 3 }
+  validates :difficulty, presence: true, length: { minimum: 3 }
+  validate :photos_type
+
+  def photos_type
+    if photos.attached? == false
+      errors.add(:photo, ": at least one photo should be attached!")
+    end
+  end
+  # ================================================================
   def duration_in_minutes
     ((end_time - start_time) / 60).to_i
   end
